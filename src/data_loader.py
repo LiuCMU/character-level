bd9f27fa7f46994cdc55c52963eee5af2254019d
Zhen Liu <liu5@andrew.cmu.edu>
2020-08-27 10:17:09 -0400

    sort code

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,7 +6,6 @@ from torch.utils.data import Dataset
 import pandas as pd
 from tqdm import tqdm
 from . import utils # import modules from sibling packages https://docs.python.org/3.4/tutorial/modules.html#importing-from-a-package
-# test git
 
 import torch
 
